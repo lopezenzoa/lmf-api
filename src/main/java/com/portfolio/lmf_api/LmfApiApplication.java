0c480228b594a6e421778af0b736ca1a7a24de0b
lopezenzoa <enzoagustinlopez2003@gmail.com>
2026-06-01 23:28:44 -0300

    initial commit

---
+++ src/main/java/com/portfolio/lmf_api/LmfApiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.lmf_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmfApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/lmf_api/LmfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.lmf_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
