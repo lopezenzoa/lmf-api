7c19aab93d8bfa3e0680a7d2dd51090056af9360
lopezenzoa <enzoagustinlopez2003@gmail.com>
2026-06-02 12:57:20 -0300

    feature: add Data Transfer Objects classes related to Court, Match and Payment entities

---
+++ src/main/java/com/portfolio/lmf_api/dto/MatchDTO.java
@@ -0,0 +1,19 @@
+package com.portfolio.lmf_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MatchDTO {
+    private LocalDateTime date;
+    private String divisionName;
+    private String homeTeamName;
+    private String visitTeamName;
+}
---
+++ src/main/java/com/portfolio/lmf_api/dto/RequestPaymentDTO.java
@@ -0,0 +1,15 @@
+package com.portfolio.lmf_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RequestPaymentDTO {
+    private Integer amount;
+    private Integer matchId;
+}
