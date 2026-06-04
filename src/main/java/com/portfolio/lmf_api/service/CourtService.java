a199196c02ae16b3fe94b348b8d30a9ed7259762
lopezenzoa <enzoagustinlopez2003@gmail.com>
2026-06-03 23:52:20 -0300

    fix: remove service interfaces unneeded and its implementation for both Court and Match entities

--- src/main/java/com/portfolio/lmf_api/service/CourtService.java
+++ src/main/java/com/portfolio/lmf_api/service/CourtService.java
@@ -2,13 +2,105 @@ package com.portfolio.lmf_api.service;
 
 import com.portfolio.lmf_api.dto.RequestCourtDTO;
 import com.portfolio.lmf_api.dto.ResponseCourtDTO;
+import com.portfolio.lmf_api.model.Court;
+import com.portfolio.lmf_api.repository.CourtRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public interface CourtService {
-    ResponseCourtDTO addCourt(RequestCourtDTO request);
-    ResponseCourtDTO updateCourt(String courtName, RequestCourtDTO request);
-    List<ResponseCourtDTO> getAll();
-    Optional<ResponseCourtDTO> getByName(String name);
+@Service
+public class CourtService {
+    @Autowired
+    private CourtRepository repository;
+
+    public ResponseCourtDTO addCourt(RequestCourtDTO request) {
+        /* Building the Entity */
+        Court court = new Court();
+        court.setName(request.getName().trim().toUpperCase());
+        court.setAddress(request.getAddress().trim().toUpperCase());
+        court.setOwnerTeamName(request.getOwnerTeamName().trim().toUpperCase());
+        court.setMatches(new ArrayList<>()); // By default, creating a court means no matches
+
+        /* Saving the Entity */
+        Court savedEntity = repository.save(court);
+
+        /* Building the Response DTO */
+        ResponseCourtDTO responseCourtDTO = new ResponseCourtDTO();
+
+        responseCourtDTO.setName(savedEntity.getName());
+        responseCourtDTO.setAddress(savedEntity.getAddress());
+        responseCourtDTO.setOwnerTeamName(savedEntity.getOwnerTeamName());
+        responseCourtDTO.setMatches(new ArrayList<>());
+
+        return responseCourtDTO;
+    }
+
+    public ResponseCourtDTO updateCourt(String courtName, RequestCourtDTO request) {
+        /* Searching for coincides with the name */
+        Optional<Court> courtOptional = repository.findByName(courtName.trim().toUpperCase());
+
+        if (courtOptional.isEmpty())
+            return null;
+
+        /* Building the Entity */
+        Court court = courtOptional.get();
+
+        court.setName(request.getName().trim().toUpperCase());
+        court.setAddress(request.getAddress().trim().toUpperCase());
+        court.setOwnerTeamName(request.getOwnerTeamName().trim().toUpperCase());
+        court.setMatches(court.getMatches()); // By default, creating a court means no matches
+
+        /* Updating the Entity */
+        Court updatedEntity = repository.save(court);
+
+        /* Building the Response DTO */
+        ResponseCourtDTO responseCourtDTO = new ResponseCourtDTO();
+
+        responseCourtDTO.setName(updatedEntity.getName());
+        responseCourtDTO.setAddress(updatedEntity.getAddress());
+        responseCourtDTO.setOwnerTeamName(updatedEntity.getOwnerTeamName());
+        responseCourtDTO.setMatches(new ArrayList<>());
+
+        return responseCourtDTO;
+    }
+
+    public List<ResponseCourtDTO> getAll() {
+        List<Court> courts = repository.findAll();
+        List<ResponseCourtDTO> responseCourtDTOS = new ArrayList<>();
+
+        for (Court court : courts) {
+            /* Building the response DTO */
+            ResponseCourtDTO responseCourtDTO = new ResponseCourtDTO();
+
+            responseCourtDTO.setName(court.getName());
+            responseCourtDTO.setAddress(court.getAddress());
+            responseCourtDTO.setOwnerTeamName(court.getOwnerTeamName());
+            responseCourtDTO.setMatches(new ArrayList<>());
+
+            responseCourtDTOS.add(responseCourtDTO);
+        }
+
+        return responseCourtDTOS;
+    }
+
+    public Optional<ResponseCourtDTO> getByName(String name) {
+        Optional<Court> courtOptional = repository.findByName(name.trim().toUpperCase());
+
+        if (courtOptional.isEmpty())
+            return Optional.empty();
+
+        /* Building the response DTO */
+        Court court = courtOptional.get();
+        ResponseCourtDTO responseCourtDTO = new ResponseCourtDTO();
+
+        responseCourtDTO.setName(court.getName());
+        responseCourtDTO.setAddress(court.getAddress());
+        responseCourtDTO.setOwnerTeamName(court.getOwnerTeamName());
+        responseCourtDTO.setMatches(new ArrayList<>());
+
+        return Optional.of(responseCourtDTO);
+    }
 }
--- src/main/java/com/portfolio/lmf_api/service/MatchService.java
+++ src/main/java/com/portfolio/lmf_api/service/MatchService.java
@@ -1,16 +1,143 @@
 package com.portfolio.lmf_api.service;
 
 import com.portfolio.lmf_api.dto.MatchDTO;
+import com.portfolio.lmf_api.model.Match;
+import com.portfolio.lmf_api.repository.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public interface MatchService {
-    MatchDTO addMatch(MatchDTO request);
-    MatchDTO updateMatch(Long matchId, MatchDTO request);
-    MatchDTO getById(Long id);
-    List<MatchDTO> getByCourtName(String courtName);
-    List<MatchDTO> getByDate(LocalDateTime date);
+public class MatchService {
+    @Autowired
+    private MatchRepository repository;
+    @Autowired private CourtService courtService;
+
+    public MatchDTO addMatch(MatchDTO request) {
+        /* Building the Entity */
+        Match entity = new Match();
+
+        entity.setDate(request.getDate());
+        entity.setDivisionName(request.getDivisionName().trim().toUpperCase());
+        entity.setHomeTeamName(request.getHomeTeamName().trim().toUpperCase());
+        entity.setVisitTeamName(request.getVisitTeamName().trim().toUpperCase());
+        entity.setPayments(new ArrayList<>());
+
+        /* Searching the Court */
+
+        /* Saving the Entity */
+        Match savedEntity = repository.save(entity);
+
+        /* Building the Response DTO */
+        MatchDTO matchDTO = new MatchDTO();
+
+        matchDTO.setDate(savedEntity.getDate());
+        matchDTO.setDivisionName(savedEntity.getDivisionName());
+        matchDTO.setHomeTeamName(savedEntity.getHomeTeamName());
+        matchDTO.setVisitTeamName(savedEntity.getVisitTeamName());
+
+        return matchDTO;
+    }
+
+    public MatchDTO updateMatch(Long matchId, MatchDTO request) {
+        /* Searching for coincidence with id */
+        Optional<Match> matchOptional = repository.findById(matchId);
+
+        if (matchOptional.isEmpty())
+            return null;
+
+        /* Building the Entity */
+        Match entity = new Match();
+
+        entity.setDate(request.getDate());
+        entity.setDivisionName(request.getDivisionName().trim().toUpperCase());
+        entity.setHomeTeamName(request.getHomeTeamName().trim().toUpperCase());
+        entity.setVisitTeamName(request.getVisitTeamName().trim().toUpperCase());
+        entity.setPayments(new ArrayList<>());
+
+        /* Searching the Court */
+
+        /* Updating the Entity */
+        Match updatedEntity = repository.save(entity);
+
+        /* Building the Response DTO */
+        MatchDTO matchDTO = new MatchDTO();
+
+        matchDTO.setDate(updatedEntity.getDate());
+        matchDTO.setDivisionName(updatedEntity.getDivisionName());
+        matchDTO.setHomeTeamName(updatedEntity.getHomeTeamName());
+        matchDTO.setVisitTeamName(updatedEntity.getVisitTeamName());
+
+        return matchDTO;
+    }
+
+    public MatchDTO getById(Long id) {
+        Optional<Match> matchOptional = repository.findById(id);
+
+        if (matchOptional.isEmpty())
+            return null;
+
+        /* Building the Response DTO */
+        MatchDTO matchDTO = new MatchDTO();
+        Match match = matchOptional.get();
+
+        matchDTO.setDate(match.getDate());
+        matchDTO.setDivisionName(match.getDivisionName());
+        matchDTO.setHomeTeamName(match.getHomeTeamName());
+        matchDTO.setVisitTeamName(match.getVisitTeamName());
+
+        return matchDTO;
+    }
+
+    public List<MatchDTO> getByCourtName(String courtName) {
+        List<Match> matches = repository.findAll();
+
+        /* Filtering with Court */
+        List<Match> matchesFiltered = new ArrayList<>();
+
+        for (Match match : matches) {
+            if (match.getCourt().getName().equals(courtName.trim().toUpperCase()))
+                matchesFiltered.add(match);
+        }
+
+        /* Building the Response DTO list */
+        List<MatchDTO> responseDTOList = new ArrayList<>();
+
+        for (Match match : matchesFiltered) {
+            MatchDTO matchDTO = new MatchDTO();
+
+            matchDTO.setDate(match.getDate());
+            matchDTO.setDivisionName(match.getDivisionName());
+            matchDTO.setHomeTeamName(match.getHomeTeamName());
+            matchDTO.setVisitTeamName(match.getVisitTeamName());
+
+            responseDTOList.add(matchDTO);
+        }
+
+        return responseDTOList;
+    }
+
+    public List<MatchDTO> getByDate(LocalDateTime date) {
+        List<Match> matches = repository.findByDate(date);
+
+        /* Building the Response DTO list */
+        List<MatchDTO> responseDTOList = new ArrayList<>();
+
+        for (Match match : matches) {
+            MatchDTO matchDTO = new MatchDTO();
+
+            matchDTO.setDate(match.getDate());
+            matchDTO.setDivisionName(match.getDivisionName());
+            matchDTO.setHomeTeamName(match.getHomeTeamName());
+            matchDTO.setVisitTeamName(match.getVisitTeamName());
+
+            responseDTOList.add(matchDTO);
+        }
+
+        return responseDTOList;
+    }
 }
